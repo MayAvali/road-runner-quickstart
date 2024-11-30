71438b3ac54effe426a7d9d2da6cad69ebe60a8b
FloofyMay <javithevali@gmail.com>
2024-11-30 17:42:25 -0600

    gurt

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/May/lib/subsystems/ManipulationSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/May/lib/subsystems/ManipulationSubsystem.java
@@ -3,14 +3,35 @@ package org.firstinspires.ftc.teamcode.May.lib.subsystems;
 import org.firstinspires.ftc.teamcode.May.lib.subsystems.subsubsystems.SlidesSubsystem;
 import org.firstinspires.ftc.teamcode.May.lib.subsystems.subsubsystems.GripperSubsystem;
 public class ManipulationSubsystem {
-    private final SlidesSubsystem slideSub;
-    private final GripperSubsystem gripperSub;
+    private final SlidesSubsystem objSlideSub;
+    private final GripperSubsystem objGripperSub;
+
+    int varSlidesClimbState = 0;
     public ManipulationSubsystem(SlidesSubsystem slideSub, GripperSubsystem gripperSub) {
-        this.slideSub = slideSub;
-        this.gripperSub = gripperSub;
+        this.objSlideSub = slideSub;
+        this.objGripperSub = gripperSub;
     }
-    public void slidesClimbStage1() {
-        slideSub.setDualSliderTarget(400);
-        slideSub.setDualRotatorTarget(650);
+    public void slidesClimbStages() {
+
+        int maxSlidesClimbState = 2;
+
+        varSlidesClimbState++;
+
+        if (varSlidesClimbState == maxSlidesClimbState) {
+            varSlidesClimbState = 0;
+        }
+
+        switch (varSlidesClimbState) {
+            case 1:
+                objSlideSub.setDualSliderTarget(1500);
+                objSlideSub.setDualRotatorTarget(150);
+                break;
+            case 2:
+                objSlideSub.setDualSliderTarget(0);
+                objSlideSub.setDualRotatorTarget(80);
+                break;
+            default:
+                break;
+        }
     }
 }
