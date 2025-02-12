7b00071b31f062c6586a4944e628bb08e38cc872
FloofyMay <javithevali@gmail.com>
2025-02-11 23:17:45 -0600

    further meepmeep refinement  pt 3

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -26,16 +26,24 @@ public class MeepMeepTesting {
                 .waitSeconds(1)
                 
                 .setTangent(Math.toRadians(270))
-                .splineToSplineHeading(new Pose2d(35,-33, Math.toRadians(-90)), Math.toRadians(90))
+                .splineToSplineHeading(new Pose2d(35,-33, Math.toRadians(-45)), Math.toRadians(90))
                 .splineToLinearHeading(new Pose2d(38,-21, Math.toRadians(-90)), Math.toRadians(90))
 
+                //Push Sample 1
+
                 .splineToSplineHeading(new Pose2d(46.00, -13.00, Math.toRadians(270)), Math.toRadians(270))
+
+                //Push Sample 2
+
                 .splineTo(new Vector2d(46.00, -55.00), Math.toRadians(270))
-                .strafeTo(new Vector2d(46.00, -13.00))
+                .strafeTo(new Vector2d(49.00, -13.00))
                 .strafeTo(new Vector2d(56.00, -13.00))
                 .strafeTo(new Vector2d(56.00, -55.00))
+
                 .lineToY(-13)
+                // Push Sample 3
                 .strafeTo(new Vector2d(61.30, -13.00))
+
                 .setTangent(Math.toRadians(270))
                 .splineToLinearHeading(new Pose2d(61.3, -45, Math.toRadians(-90)), Math.toRadians(-90))
                 .splineToLinearHeading(new Pose2d(46.3, -62, Math.toRadians(-90)), Math.toRadians(-90))
