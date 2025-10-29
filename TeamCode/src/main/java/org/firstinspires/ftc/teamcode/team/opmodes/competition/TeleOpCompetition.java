32fb4c435e6198cbd83867dafc7304b4cee29415
May <121705799+MayAvali@users.noreply.github.com>
2025-10-28 22:04:33 -0500

    pid tuning implemented for launcher. intake pid tuning exists but is not optimal. dashboard has been integrated

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team/opmodes/competition/TeleOpCompetition.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team/opmodes/competition/TeleOpCompetition.java
@@ -115,7 +115,7 @@ public class TeleOpCompetition extends LinearOpMode {
 
                     drivetrain.botOrientedDrive(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x, 0);
 
-                    ScoringSystem.intake(gamepad1.left_trigger, gamepad1.right_trigger*0.475);
+                    ScoringSystem.intake(gamepad1.left_trigger, gamepad1.right_trigger*0.525);
 
                     if (launcherAccel.isPressed()) {
                         ScoringSystem.launchAccel();
