dddfdbc0520f0ee7aa5d25730f2f197c9b91f18b
May <121705799+MayAvali@users.noreply.github.com>
2026-01-14 22:22:18 -0600

    cleaned, fixed mecanumDT

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team/subsystems/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team/subsystems/MecanumDrive.java
@@ -24,7 +24,7 @@ public class MecanumDrive {
         // Without this, the REV Hub's orientation is assumed to be logo up / USB
         // forward
         imu.initialize(parameters);
-        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
         leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
@@ -32,7 +32,7 @@ public class MecanumDrive {
         imu.resetYaw();
     }
 
-    public void botOrientedDrive(double y, double x, double rx, double sp) {
+    public void botOrientedDrive(double x, double y, double rx, double sp) {
         // This button choice was made so that it is hard to hit on accident,
         // it can be freely changed based on preference.
         // The equivalent button is start on Xbox-style controllers.
@@ -41,10 +41,10 @@ public class MecanumDrive {
         // This ensures all the powers maintain the same ratio,
         // but only if at least one is out of the range [-1, 1]
         double denominator = Math.max(Math.abs(rx) + Math.abs(x) + Math.abs(y), 1);
-        double frontLeftPower = ((-rx - x - y) / denominator) * (1 - (0.6 * sp));
-        double backLeftPower = ((-rx + x - y) / denominator) * (1 - (0.6 * sp));
-        double frontRightPower = ((-rx - x + y) / denominator) * (1 - (0.6 * sp));
-        double backRightPower = ((-rx + x + y) / denominator) * (1 - (0.6 * sp));
+        double frontLeftPower = ((-y + x + rx) / denominator) * (1 - (0.6 * sp));
+        double backLeftPower = ((-y - x + rx) / denominator) * (1 - (0.6 * sp));
+        double frontRightPower = ((-y - x - rx) / denominator) * (1 - (0.6 * sp));
+        double backRightPower = ((-y + x - rx) / denominator) * (1 - (0.6 * sp));
 
         leftFront.setPower(frontLeftPower);
         leftBack.setPower(backLeftPower);
