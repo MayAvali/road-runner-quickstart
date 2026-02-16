2ea1bf8d6b6ceed72571065da20ec3eee11296c8
FloofyMay <javithevali@gmail.com>
2026-02-16 16:52:51 -0600

    neew stuff for better auto maybe...
    also turret fix

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team/opmodes/competition/autonomous/RedAutoRedux.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode.team.opmodes.competition.autonomous;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
+import org.firstinspires.ftc.teamcode.team.subsystems.ScoringSystem;
+import org.firstinspires.ftc.teamcode.team.subsystems.ServoGate;
+@Autonomous(name = "RedCornerAutoAllRedux", group = "Autonomous OpMode")
+public class RedAutoRedux extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        Telemetry dashboardTelemetry = dashboard.getTelemetry();
+
+        ScoringSystem ScoringSystem = new ScoringSystem(
+                (DcMotorEx) hardwareMap.dcMotor.get("launcher"),
+                (DcMotorEx) hardwareMap.dcMotor.get("intake"),
+                (DcMotorEx) hardwareMap.dcMotor.get("turret"),
+                (DcMotorEx) hardwareMap.dcMotor.get("launcher2")
+        );
+        ServoGate ServoGate = new ServoGate(
+                hardwareMap.servo.get("gate")
+        );
+    }
+}
