7c3e63bbfc5917d9fdd67912765781b7d7f24ebd
May <121705799+MayAvali@users.noreply.github.com>
2026-02-19 02:08:15 -0600

    autonomous names r nicer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team/opmodes/competition/autonomous/BlueAutoAll.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team/opmodes/competition/autonomous/BlueAutoAll.java
@@ -15,7 +15,7 @@ import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
 import org.firstinspires.ftc.teamcode.team.subsystems.ScoringSystem;
 import org.firstinspires.ftc.teamcode.team.subsystems.ServoGate;
 
-@Autonomous(name = "BlueCornerAutoAll", group = "Autonomous OpMode")
+@Autonomous(name = "BlueAutoAll", group = "Autonomous OpMode")
 public class BlueAutoAll extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team/opmodes/competition/autonomous/RedAutoAll.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team/opmodes/competition/autonomous/RedAutoAll.java
@@ -15,7 +15,7 @@ import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
 import org.firstinspires.ftc.teamcode.team.subsystems.ScoringSystem;
 import org.firstinspires.ftc.teamcode.team.subsystems.ServoGate;
 
-@Autonomous(name = "RedCornerAutoAll", group = "Autonomous OpMode")
+@Autonomous(name = "RedAutoAll", group = "Autonomous OpMode")
 public class RedAutoAll extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
